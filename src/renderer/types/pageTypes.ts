65e816eff0a91a6902f3f0aecf0ef3b9919248e6
童俊 <tongjun741@126.com>
2024-01-04 15:30:39 +0800

    测试构建

--- src/renderer/types/pageTypes.ts
+++
@@ -1,1 +0,0 @@
-export type PageTypes = 'my' | 'favorite' | 'team';
--- src/renderer/utils/formatUtils.ts
+++
@@ -1,4 +0,0 @@
-export function formatNumber(num: number) {
-  if (Math.abs(num) < 1000) return num;
-  return `${Math.sign(num) * (Math.round(Math.abs(num) / 100) / 10)}k`;
-}
